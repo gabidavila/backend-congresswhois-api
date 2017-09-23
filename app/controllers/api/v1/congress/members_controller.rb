cd7f7cb51e75fa655e5569efd9ad0aabe3357b32
gabidavila <gabidavila@gmail.com>
2017-09-23 18:21:16 -0400

    refactored to use scopes

--- app/controllers/api/v1/congress/members_controller.rb
+++ app/controllers/api/v1/congress/members_controller.rb
@@ -1,32 +1,25 @@
 class Api::V1::Congress::MembersController < ApplicationController
 
   def index
-    limit = params[:limit] || 12
-
-    party = params[:party]
-    party = ["R", "D", "I"] if !params[:party] || params[:party] == "A"
-
-    @congress = CongressMember.where(party: party).order_name.limit(limit)
-    render :json => @congress
+    render :json => fetch("order_name")
   end
 
   def senate
-    limit = params[:limit] || 12
-
-    party = params[:party]
-    party = ["R", "D", "I"] if !params[:party] || params[:party] == "A"
-
-    @congress = CongressMember.where(party: party).senate.limit(limit)
-    render :json => @congress
+    render :json => fetch("senate")
   end
 
   def house
+    render :json => fetch("house")
+  end
+
+  private
+
+  def fetch(scope)
     limit = params[:limit] || 12
 
     party = params[:party]
     party = ["R", "D", "I"] if !params[:party] || params[:party] == "A"
 
-    @congress = CongressMember.where(party: party).house.limit(limit)
-    render :json => @congress
+    @congress = CongressMember.where(party: party).send(scope).limit(limit)
   end
 end
