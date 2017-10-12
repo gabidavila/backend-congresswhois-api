59e1669e8ea87ffd878116a3f02cc6f1a586450f
gabidavila <gabidavila@gmail.com>
2017-10-12 09:50:42 -0400

    changed endpoint to accept congress as params

--- app/controllers/api/v1/congress/members_controller.rb
+++ app/controllers/api/v1/congress/members_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Congress::MembersController < ApplicationController
   def index
-    render json: fetch('order_name')
+    render json: fetch
   end
 
   def senate
@@ -18,15 +18,15 @@ class Api::V1::Congress::MembersController < ApplicationController
 
   private
 
-  def fetch(scope)
+  def fetch(scope = 'order_name')
     limit = params[:limit] || 12
 
     party = params[:party]
     party = %w{R D I} if !params[:party] || params[:party] == 'A'
 
     name ||= params[:name]
-
     state ||= params[:state]
+    scope = params[:congress] if params[:congress] && params[:congress] != ''
 
     if state == 'A'
       state = nil
