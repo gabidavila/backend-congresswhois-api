bc448a84b78195da786c6c4611baff71aa0dc7c0
gabidavila <gabidavila@gmail.com>
2017-10-19 17:13:00 -0400

    added extra parameters for comparison

--- app/controllers/api/v1/congress/comparisons_controller.rb
+++ app/controllers/api/v1/congress/comparisons_controller.rb
@@ -2,7 +2,9 @@ class Api::V1::Congress::ComparisonsController < ApplicationController
   def compare
     render json: {
       votes: ProPublica::Congress::Comparisons.votes(params[:comparison]),
-      bills: ProPublica::Congress::Comparisons.bills(params[:comparison])
+      bills: ProPublica::Congress::Comparisons.bills(params[:comparison]),
+      chamber: params[:comparison][:chamber],
+      congress: params[:comparison][:congress]
     }
   end
 end
