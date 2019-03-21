a844ed373cc1019b694a0dc06bb689f6d8d5c7a1
gabidavila <gabidavila@gmail.com>
2019-03-21 19:33:39 -0400

    fixing current congress on zipcode search

--- app/controllers/api/v1/zipcodes/districts_controller.rb
+++ app/controllers/api/v1/zipcodes/districts_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Zipcodes::DistrictsController < ApplicationController
   def search
-    members = CongressMember.zipcodes.where(zipcodes_districts: {zipcode: params['zipcode']}).page(1)
+    members = CongressMember.zipcodes.where(zipcodes_districts: {zipcode: params['zipcode']}).where(congress: ProPublica::Congress.current).page(1)
     render json: members, meta: pagination_dict(members)
   end
 end
